ec97d5ebe8b56b19e32f5208b0f63f4ac83752d0
MichaelSornoza <mospmanta@gmail.com>
2020-09-15 19:38:00 -0500

    Clase de If

---
+++ Calculadora/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+
+	operacion := scanner.Text()
+
+	fmt.Println(operacion)
+
+	valores := strings.Split(operacion, "+")
+
+	fmt.Println(valores)
+
+	operador1, err1 := strconv.Atoi(valores[0])
+
+	if err1 != nil {
+		fmt.Println(err1)
+	} else {
+		fmt.Println(operador1)
+	}
+
+	operador2, _ := strconv.Atoi(valores[1])
+
+	fmt.Println(operador1 + operador2)
+}
